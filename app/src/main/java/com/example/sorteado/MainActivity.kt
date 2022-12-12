b0ea31da1f18bb8b182bfe6246799b0bbe3aef88
Alicia Alexia <alicia.alexia03@gmail.com>
2022-12-12 12:35:46 -0300

    renaming constant name, suitable way of declaring text

--- app/src/main/java/com/example/sorteado/MainActivity.kt
+++ app/src/main/java/com/example/sorteado/MainActivity.kt
@@ -5,32 +5,31 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import kotlin.random.Random.Default.nextInt
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val sortear : Button = findViewById(R.id.button_sortear)
-        sortear.setOnClickListener { SortearNumeros() }
+        val sorter : Button = findViewById(R.id.button_sortear)
+        sorter.setOnClickListener { sorterNumerous() }
 
         val reset : Button = findViewById(R.id.button_resetar)
-        reset.setOnClickListener { ResetarNumeros() }
+        reset.setOnClickListener { resetNumerous() }
     }
-    private fun SortearNumeros() {
-        val numero_inicial: EditText = findViewById(R.id.edit_digite_numero)
-        val resultado : TextView = findViewById(R.id.text_resultado)
-        val inicial = Integer.parseInt(numero_inicial.getText().toString())
-        val sorteador = nextInt(0, inicial) +1
-        resultado.text = "Número sorteado foi: " + sorteador
-        resultado.contentDescription = sorteador.toString()
+    private fun sorterNumerous() {
+        val numberInitial: EditText = findViewById(R.id.edit_digite_numero)
+        val result : TextView = findViewById(R.id.text_resultado)
+        val initial = Integer.parseInt(numberInitial.text.toString())
+        val sorted = Random.nextInt(0, initial) +1
+        "Número sorteado foi: $sorted".also { result.text = it }
     }
 
-    private fun ResetarNumeros(){
-        val numero_inicial: EditText = findViewById(R.id.edit_digite_numero)
-        numero_inicial.text.clear()
-        val resultado : TextView = findViewById(R.id.text_resultado)
-        resultado.text = "Nenhum número selecionado: "
+    private fun resetNumerous(){
+        val numberInitial: EditText = findViewById(R.id.edit_digite_numero)
+        numberInitial.text.clear()
+        val result : TextView = findViewById(R.id.text_resultado)
+        "Nenhum número selecionado: ".also { result.text = it }
     }
 
 }
